fa78217a73a5e372142988bbbf0c4306760b16ee
Tyler Harper <tylerdh12@gmail.com>
2020-09-02 13:57:35 -0700

    change to functional component add loading indi...

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -1,65 +1,67 @@
 import React from "react";
-import Course from "./Course";
 import config from "../../config";
+import Course from "./Course";
 
-export default class Courses extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      courses: [],
-      errors: []
-    };
-  }
+const Courses = ({ history }) => {
+  const [courses, updateCourses] = React.useState([]);
+  const [errors, updateErrors] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
-  componentDidMount() {
-    this.getCourses();
-  }
-
-  // Get Courses Event Handler
-  getCourses() {
+  React.useEffect(() => {
+    setLoading(true);
     fetch(config.apiBaseUrl + "/courses")
-      .then(res => {
+      .then((res) => {
         if (res.status === 500) {
-          this.props.history.push("/error");
+          updateErrors("500");
+          history.push("/error");
         } else {
           return res.json();
         }
       })
-      .then(data => {
-        this.setState({
-          courses: data
-        });
+      .then((data) => {
+        updateCourses(data);
+        setLoading(false);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="bounds">
-        {this.state.courses.map(course => (
-          <Course title={course.title} id={course.id} key={course.id} />
-        ))}
-
-        <div className="grid-33">
-          <a
-            className="course--module course--add--module"
-            href="courses/create"
-          >
-            <h3 className="course--add--title">
-              <svg
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                viewBox="0 0 13 13"
-                className="add"
-              >
-                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-              </svg>
-              New Course
-            </h3>
-          </a>
-        </div>
+  return loading ? (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        height: "80vh",
+      }}
+    >
+      <h1 style={{ textAlign: "center", fontSize: 24, color: "grey" }}>
+        Loading . . .
+      </h1>
+    </div>
+  ) : (
+    <div className="bounds">
+      {courses.map((course) => (
+        <Course title={course.title} id={course.id} key={course.id} />
+      ))}
+      <div className="grid-33">
+        <a className="course--module course--add--module" href="courses/create">
+          <h3 className="course--add--title">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 13 13"
+              className="add"
+            >
+              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+            </svg>
+            New Course
+          </h3>
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Courses;

d1394860eb337986f79bd13292836caf17cf3104
Tyler Harper <tylerdh12@gmail.com>
2020-09-25 12:54:58 -0700

    remove comma

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./global.css";
-
-import { Provider } from "./Context";
 import App from "./App";
+import { Provider } from "./Context";
+import "./global.css";
 
 ReactDOM.render(
   <Provider>
